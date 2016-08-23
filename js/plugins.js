1ce5a2ed1101d0225c0bd184ce6e47d113f22f50
ZojaHara <basileus1900@gmail.com>
2016-08-23 16:34:56 +0200

    version complete

---
+++ js/plugins.js
@@ -0,0 +1,20 @@
+
+
+(function($) {
+ 
+	$(document).ready(function(){
+  		$('.bxslider').bxSlider({
+  			auto:true,
+  			speed:1000,
+  			adaptiveHeight:true,
+  			controls:false,
+  			autoHover:true
+  		});
+
+});
+
+   //Loader animation
+	$('#loader').delay(2200).fadeOut(1500).css({'overflow':'visible'}); 
+
+
+})(jQuery)
---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+;(function($) {
+
+	$(document).ready(function() { 
+ 
+ 		$('.jsScroll').on('click', function(event) {
+ 
+ 		var target = $( $(this).attr('href') );
+ 
+ 			if( target.length ) {
+ 				event.preventDefault();
+ 				$('html, body').animate({
+ 				scrollTop: target.offset().top
+ 			}, 700);
+ 		}
+ 	});
+ 
+});
+ 
+
+
+	$(".close-cross").on("click", function() {
+		$(".about-me").slideUp(1000);
+		$("body").removeClass("overflow-y");
+	});
+
+
+	$(".presentation-contact").on("click", function() {
+		$("body").removeClass("overflow-y");
+		$(".about-me").slideUp(100);
+	});
+
+
+	//Cricles animation
+
+	$(".skills-container").on("click", function(e) {
+		e.preventDefault;
+		$(this).toggleClass("paused").addClass("skills-container-animation");
+	});
+
+	$(".skills-arrow:nth-child(6)").on("click", function() {
+		$(".skills-container").removeClass("paused").removeClass("skills-container-animation-2").addClass("skills-container-animation");
+	});
+
+	$(".skills-arrow:nth-child(7)").on("click", function() {
+		$(".skills-container").removeClass("paused").removeClass("skills-container-animation").addClass("skills-container-animation-2");
+	});
+
+})(jQuery);
+
+
+
+
