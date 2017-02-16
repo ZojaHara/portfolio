8716fba2e0d10bdc56915225eac0788fb2f303b9
ZojaHara <k.harasimiuk@gmail.com>
2017-02-16 11:28:49 +0100

    section-tip added / color changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 ;(function($) {
 
-//Loader animation
+//Loader animations
 	$('.loader').delay(1900).fadeOut(1500).css({'overflow':'visible'});
 	$(".loader-view").on("click", function() {
 		$(".main-page-loader").css("display", "flex").delay(1800).fadeOut(700).css({'overflow':'visible'});
 	});
 
 	$(".close-cross, .presentation-contact, .menu-list").on("click", function() {
-		$(".intro").slideUp(400);
+		$(".intro").slideUp(300);
 		$(".slide_menu").fadeOut(500);
 	});
 
@@ -15,9 +15,18 @@
 		$(".intro").slideDown(400);
 	});
 
-
 	$(".hamburger-container").on("click", function() {
 		$(".slide_menu").slideDown(700).css("display","flex");
+	});
+
+	// Projects
+
+	$(".question-mark-container").on("click", function() {
+		$(".tip-section").slideDown((500));
+	});
+
+	$(".close-cross-tip").on("click", function() {
+		$(".tip-section").fadeOut(500);
 	});
 
 	//Cricles animation
